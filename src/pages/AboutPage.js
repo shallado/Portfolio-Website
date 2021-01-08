0944b04c60f08ca6b44909436c8191cfae86e0b5
Sandro Hallado <sandrohallado@yahoo.com>
2021-01-07 18:07:44 -0800

    add about page contents

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,5 +1,33 @@
 function AboutPage() {
-  return <div>About Page</div>;
+  return (
+    <div>
+      <nav>
+        <div>
+          <p>headshot img</p>
+        </div>
+        <ul>
+          <li>About</li>
+          <li>Work</li>
+          <li>Technologies</li>
+          <li>Contact</li>
+        </ul>
+        <div>social media</div>
+      </nav>
+      <div>
+        <h2>About</h2>
+        <h1>
+          <span>Sandro Hallado</span>
+          <span>Full Stack Developer</span>
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+          consectetur asperiores impedit doloremque! Placeat consequatur quae
+          cum a non, voluptatum quia accusamus, assumenda quaerat dolorum saepe
+          nobis, doloribus voluptatem fuga!
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default AboutPage;
