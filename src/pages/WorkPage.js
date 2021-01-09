2292acd84494e502c52e56ce59026c4468c321a7
Sandro Hallado <sandrohallado@yahoo.com>
2021-01-08 19:49:56 -0800

    add styling to the work page

--- src/pages/WorkPage.js
+++ src/pages/WorkPage.js
@@ -1,13 +1,40 @@
+import React from 'react';
+import project from '../img/project.png';
+
 function WorkPage() {
   return (
     <div className="work">
-      <h2>Work</h2>
-      <div>
+      <h2 className="heading-two">Work</h2>
+      <div className="projects">
         <div>
-          <div>
-            <div>Porfolio Project</div>
+          <div className="projects__project-img-container">
+            <img
+              className="projects__project-img"
+              src={project}
+              alt="project 1"
+            />
           </div>
-          <h3>Portfolio Project Name</h3>
+          <h3 className="heading-three">Project 1</h3>
+        </div>
+        <div>
+          <div className="projects__project-img-container">
+            <img
+              className="projects__project-img"
+              src={project}
+              alt="project 2"
+            />
+          </div>
+          <h3 className="heading-three">Project 2</h3>
+        </div>
+        <div>
+          <div className="projects__project-img-container">
+            <img
+              className="projects__project-img"
+              src={project}
+              alt="project 3"
+            />
+          </div>
+          <h3 className="heading-three">Project 3</h3>
         </div>
       </div>
     </div>
